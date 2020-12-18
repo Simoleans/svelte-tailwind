872f506051e04fc0f82b6a0848a7f603a4d1645d
Fran <francisco20990@gmail.com>
2020-12-17 20:25:19 -0400

    Package.json | agregar package cross-env para la ejecución del comando build para tailwind
    tailwind.config | agregar el purge para cuando se ejecute el build, eliminar datos sin importancia

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     purgeLayersByDefault: true
   },
   purge: ['src/*.svelte', 'src/**/*.svelte'],
+  // purge: { purge manual
+  //   enabled: true,
+  //   content : ['src/*.svelte', 'src/**/*.svelte'],
+  // },
   theme: {
     extend: {
       colors: {
@@ -24,21 +28,7 @@ module.exports = {
         'inner-b': 'inset 0 -8px rgba(0, 0, 0, 0.199)',
         win:
           '0 0 0 40px hsl(217, 16%, 45%, 0.05), 0 0 0 70px hsl(217, 16%, 45%, 0.08), 0 0 0 100px hsl(217, 16%, 45%, 0.1)'
-      },
-      gradientColorStops: theme => ({
-        'scissors-primary': '#ec9e0e',
-        'scissors-secondary': '#eca922',
-        'paper-primary': '#4865f4',
-        'paper-secondary': '#5671f5',
-        'rock-primary': '#dc2e4e',
-        'rock-secondary': '#dd405d',
-        'lizard-primary': '#834fe3',
-        'lizard-secondary': '#8c5de5',
-        'cyan-primary': '#40b9ce',
-        'cyan-secondary': '#52bed1',
-        'radial-primary': '#1f3756',
-        'radial-secondary': '#141539'
-      })
+      }
     }
   },
   variants: {},
